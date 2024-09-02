1b58269ba32769f7b2ab3d3f3cfa40eef40dcdd0
aniket.c <aniketkv1998@gmail.com>
2024-09-02 17:10:38 +0530

    Added the location filter for JPMC jobs

--- src/routes/JpmcRouter.js
+++ src/routes/JpmcRouter.js
@@ -11,17 +11,46 @@ router.get("/jobs", async (req, res, next) => {
       utilityService.server.locals.jobFetchLock.jpmc = 1;
 
       if (jpmcService.areExistingJobsTooOld()) {
-        await jpmcService.fetchJobs();  
+        await jpmcService.fetchJobs();
       }
 
-      utilityService.server.locals.jobFetchLock.jpmc = 0;        
+      utilityService.server.locals.jobFetchLock.jpmc = 0;
     }
 
     const intervalId = setInterval(() => {
       if (utilityService.server.locals.jobFetchLock.jpmc == 0) {
         clearInterval(intervalId);
 
-        res.status(200).json(utilityService.server.locals.jobs.jpmc);
+        let jobs = utilityService.server.locals.jobs.jpmc;
+
+        let responseJobs = null;
+
+        if (null === req.query.locations || req.query.locations === undefined) {
+          responseJobs = jobs;
+        } else {
+          let locations = req.query.locations.split(",");
+          responseJobs = jobs.filter((job) => {
+            let trueFlag = locations.some((location) => {
+              let flag = job.primaryLocation
+                .toLowerCase()
+                .includes(location.toLowerCase());
+
+              if (!flag) {
+                flag = job.secondaryLocations.some((secondaryLocation) => {
+                  secondaryLocation.name
+                    .toLowerCase()
+                    .includes(location.toLowerCase());
+                });
+              }
+
+              return flag;
+            });
+
+            return trueFlag;
+          });
+        }
+
+        res.status(200).json(responseJobs);
       }
     }, 0);
   } catch (err) {
